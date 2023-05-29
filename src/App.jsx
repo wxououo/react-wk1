dc3d17b809d1d4f7dbe5395e7af2db0bccb07d44
wxououo <S111019010@stu.ntue.edu.tw>
2023-05-29 11:53:39 +0800

    firebase2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 import 'antd/dist/reset.css'
 import Home from './pages/Home'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 // import { feedImages } from "./api"
 // feedImages();
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
-    <Home />
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   )
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,51 +1,48 @@
-
-import { initializeApp } from "firebase/app";
-import { getFirestore , collection , setDoc , getDocs , doc , deleteDoc } from "firebase/firestore";
+import { getApps, getApp, initializeApp } from "firebase/app";
+import { getFirestore, collection, setDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
 import images from "../json/images.json";
 
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APPID
-  };
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APPID
+};
 
+const app_length = getApps().length > 0;
 
+// Initialize Firebase
+const app = app_length ? getApp() : initializeApp(firebaseConfig);
 
-  const app_length = getApps().length > 0;
+// REFERENCE DB
+const db = getFirestore(app);
 
-  // Initialize Firebase
-  const app = app_length ? getApp() : initializeApp(firebaseConfig);
-  
-  // REFERENCE DB
-  const db = getFirestore(app);
-  
-  // REFERENCE COLLECTION
-  const imagesCollection = collection(db, "image"); 
-  
-  export const feedImages = async () => {
-    // DELETE ALL EXISTING DOCS
-    const querySnapshot = await getDocs(imagesCollection);
-    querySnapshot.forEach(async (images) => {
-      await deleteDoc(doc(db, "image", images.id));
-    });
-    // ADD NEW DOCS
-    images.forEach(async (images) => {
-      const docRef = await doc(imagesCollection);
-      await setDoc(docRef, { ...images, id: docRef.id });
-    });
-  };
-  
-  export const getImages = async () => {
-      const querySnapshot = await getDocs(imagesCollection);
-      // Convert query to a json array.
-      let result = [];
-      querySnapshot.forEach(async (images) => {
-         await result.push(images.data());
-      });
-      console.log({ result });
-      return result
-   };
+// REFERENCE COLLECTION
+const imagesCollection = collection(db, "images");
+
+export const feedImages = async () => {
+  // DELETE ALL EXISTING DOCS
+  const querySnapshot = await getDocs(imagesCollection);
+  querySnapshot.forEach(async (images) => {
+    await deleteDoc(doc(db, "image", images.id));
+  });
+  // ADD NEW DOCS
+  images.forEach(async (image) => {
+    const docRef = await doc(imagesCollection);
+    await setDoc(docRef, { ...image, id: docRef.id });
+  });
+};
+
+export const getImages = async () => {
+  const querySnapshot = await getDocs(imagesCollection);
+  // Convert query to a json array.
+  let result = [];
+  querySnapshot.forEach(async (image) => {
+    await result.push(image.data());
+  });
+  console.log({ result });
+  return result
+};
